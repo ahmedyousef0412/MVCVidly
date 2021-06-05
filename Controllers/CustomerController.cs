59743c9138ac3e9d7bad6455f7013c323dc7b7a9
LajjaYousef <lajjayousef66@gmail.com>
2021-06-05 03:06:08 +0200

    Design The View Of MemberShipType and Customer and related it by [Eager Loading].

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using WebGrease.Css.Extensions;
+using  System.Data.Entity;
+
+namespace Vidly.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public CustomerController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var context = _context.Customers.
+                Include(c => c.MemberShipType).ToList();
+            return View(context);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var context = _context.Customers.Include(m=>m.MemberShipType).
+                SingleOrDefault(c => c.Id == id);
+
+
+            if (context == null)
+                return HttpNotFound();
+
+            return View(context);
+        }
+    }
+}
