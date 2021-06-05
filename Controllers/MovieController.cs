e60b7204756d999d965c1a80b3410d462d4682f6
LajjaYousef <Lajjayousef66@gmail.com>
2021-06-06 00:52:13 +0200

    Using Git hub Desktop

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MovieController : Controller
+    {
+
+        ApplicationDbContext _context;
+        public MovieController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        // GET: Movie
+        public ActionResult Index()
+        {
+            var context = _context.Movies.ToList();
+            return View();
+        }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Required] [StringLength(255)] public string Name { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public byte NumberInStock { get; set; }
+
+        public Genre Genre { get; set; }
+
+        public int GenreId { get; set; }
+    }
+}
