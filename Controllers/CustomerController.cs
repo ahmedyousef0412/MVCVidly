47aa8c489a32f0562696dc40418aa9a5a30847d2
LajjaYousef <Lajjayousef66@gmail.com>
2021-06-06 01:35:42 +0200

    Display list of Movie and their details

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -42,5 +42,7 @@ namespace Vidly.Controllers
 
             return View(context);
         }
+
+      
     }
 }
--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,20 +11,34 @@ namespace Vidly.Controllers
     public class MovieController : Controller
     {
 
-        ApplicationDbContext _context;
+       private readonly ApplicationDbContext _context;
         public MovieController()
         {
             _context = new ApplicationDbContext();
         }
         protected override void Dispose(bool disposing)
         {
+            
             _context.Dispose();
         }
         // GET: Movie
         public ActionResult Index()
         {
-            var context = _context.Movies.ToList();
-            return View();
+            var context = _context.Movies.
+                Include(m=> m.Genre).ToList();
+            return View(context);
+        }
+        public ActionResult Details(int id)
+        {
+            var movie = _context.Movies.
+                Include(m=>m.Genre).
+                SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
         }
     }
 }
